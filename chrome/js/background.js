b8ad4927428c21f67ef34c77659d9b0df4bd2a36
chunhua.chen <chunhua.chen@tcl.com>
2017-11-04 16:58:53 +0800

    add replace jsapi

--- chrome/js/background.js
+++ chrome/js/background.js
@@ -2,10 +2,12 @@ chrome.webRequest.onBeforeRequest.addListener(
     function(request) {
         var url = request.url.replace('googleapis.com', 'lug.ustc.edu.cn');
         url = url.replace('themes.googleusercontent.com', 'google-themes.lug.ustc.edu.cn');
+        url = url.replace('www.google.com', 'lib.sinaapp.com');
         return {redirectUrl: url};
     },
     {
         urls: [
+            "*://www.google.com/jsapi",
             "*://ajax.googleapis.com/*",
             "*://themes.googleusercontent.com/*"
         ]
